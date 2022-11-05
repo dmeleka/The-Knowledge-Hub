2b3d41aded4c919809e3719c5e43af27c6da1eb8
Yomna Ihab <yomna.khonfes@student.guc.edu.eg>
2022-11-05 14:30:08 +0200

    created courseFilter function added its route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,3 +59,69 @@ export const search = async (req, res) => {
     }
     res.status(404).json({ message: "Not Found" });
 }
+
+export const coursesFilter = async (req, res) => {
+    const allCourses = await Courses.find({});
+    var myCourses = [];
+    var sub = false;
+    var price = false;
+    var rating = false;
+
+    if (req.body.Subject.length != 0)
+        sub = true;
+
+    if (req.body.maxPrice.length != 0)
+        price = true;
+
+    if (req.body.Rating.length != 0)
+        rating = true;
+
+    if (sub || price || rating) {
+        for (let i = 0; i < allCourses.length; i++) {
+            if (sub && price && rating) {
+                if (req.body.maxPrice >= allCourses[i].Price && req.body.Subject == allCourses[i].Subject && req.body.Rating <= allCourses[i].Rating)
+                    myCourses.push(allCourses[i]);
+            }
+            else if (price && sub) {
+                if (req.body.maxPrice >= allCourses[i].Price && req.body.Subject == allCourses[i].Subject)
+                    myCourses.push(allCourses[i]);
+            }
+            else if (sub && rating) {
+                if (req.body.Subject == allCourses[i].Subject && req.body.Rating <= allCourses[i].Rating)
+                    myCourses.push(allCourses[i]);
+            }
+            else if (price && rating) {
+                if (req.body.maxPrice >= allCourses[i].Price && req.body.Rating <= allCourses[i].Rating)
+                    myCourses.push(allCourses[i]);
+            }
+
+            else if (sub) {
+                if (req.body.Subject == allCourses[i].Subject)
+                    myCourses.push(allCourses[i]);
+            }
+            else if (rating) {
+                if (req.body.Rating <= allCourses[i].Rating)
+                    myCourses.push(allCourses[i]);
+            }
+            else {
+                if (req.body.maxPrice >= allCourses[i].Price)
+                    myCourses.push(allCourses[i]);
+            }
+        }
+
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                myCourses
+            }
+        })
+    }
+    else {
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                allCourses
+            }
+        })
+    }
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { setCountry, login, search } from '../controllers/userController.js';
+import { setCountry, login, search, coursesFilter } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/Login', login);
 router.post('/setCountry', setCountry);
 router.get('/search', search);
+router.get('/courseFilter', coursesFilter);
 
 
 export default router;
