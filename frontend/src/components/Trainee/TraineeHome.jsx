5cf0e0986cd6c44634d0aa916957240ffa71c8c1
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2022-12-13 20:11:39 +0100

    files to be implemented

---
+++ frontend/src/components/Trainee/TraineeHome.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TraineeNavbar from "./TraineeNavBar";
+
+const TraineeHome = () => {
+    return (
+        <>
+            <TraineeNavbar />
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default TraineeHome
