9d7f0682831520b163e51430dc7eefc3ffd81b27
Yomna Ihab <yomna.khonfes@student.guc.edu.eg>
2022-12-13 21:30:17 +0200

    video &items routes

--- backend/routes/traineeRoutes.js
+++ backend/routes/traineeRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addIndTrainee,rateInstructor,rateCourse} from '../controllers/traineeController.js';
+import { addIndTrainee,rateInstructor,rateCourse,watchVideo} from '../controllers/traineeController.js';
 
 const router = express.Router();
 
 router.post('/Register', addIndTrainee);
 router.post('/rateInstructor',rateInstructor);
 router.post('/rateCourse',rateCourse);
-
+router.get('/watchvideo',watchVideo);
+router.get('/openItems',openItems);
 export default router;
