8b2e1902c7ab030e0c26ed0025fdb0267ec9629c
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2022-12-13 19:02:08 +0100

    added email to schema

--- backend/models/traineeModel.js
+++ backend/models/traineeModel.js
@@ -6,6 +6,10 @@ const traineeSchema = new Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
     password: {
         type: String,
         required: true
