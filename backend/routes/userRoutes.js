f93c7f472d65cdc5f8de8e8aa59577f3f511dab6
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2022-12-13 19:01:53 +0100

    - implemented forgotPassword & resetPassword - updated search - usage of JWT

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,18 @@
 import express from 'express';
-import { setCountry, login, search, coursesFilter, view, changePassword } from '../controllers/userController.js';
+import { setCountry, login, search, coursesFilter, view, changePassword, resetPassword, forgotPassword, verifyJWT, authResetPassword, searchView } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/setCountry', setCountry);
+router.post('/forgotPassword', forgotPassword);
 router.put('/changePassword', changePassword);
-router.get('/search', search);
+router.get('/resetPassword/:id/:token', authResetPassword);
+router.put('/resetPassword/:id/:token', resetPassword);
+router.post('/search', search);
+router.get('/search/:search', searchView);
 router.get('/courseFilter', coursesFilter);
-router.get('/view', view);
+router.get('/view/:view', view);
 
 
 export default router;
