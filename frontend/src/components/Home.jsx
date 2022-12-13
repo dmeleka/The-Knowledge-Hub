225ec4592d620b604bd1591451e9d01465f7366c
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2022-12-13 19:07:49 +0100

    Created homepage components

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,26 +2,30 @@ import React from 'react'
 import About from './About'
 import Contact from './Contact'
 import Services from './Services'
+import { NavLink } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <section id="home">
                 <div className="container">
-                    <div className="row justify-content-center">
+                    <div className="row justify-content-left">
                         <div className="col-md-8 mt-5">
-                            <h1 className="display-4 fw-bolder mb-4 text-center text-white">Testin if this actually works</h1>
-                            <p className="lead text-center fs-4 mb-5 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi quam illum omnis recusandae perferendis odio laudantium ea dicta ullam cumque.</p>
-                            <div className="buttons d-flex justify-content-center">
-                                <button className="btn btn-light me-4 rounded-pill px-4 py-2">Get Quote</button>
-                                <button className="btn btn-outline-light rounded-pill px-4 py-2">Our services</button>
+                            {/* <h1 className="display-4 fw-bolder mb-4 text-left text-black">Learn Without Limits</h1> */}
+                            <h1 className="home-head">Learn <b>Without</b> <br /> Limits</h1>
+                            <p className="lead text-left fs-6 mb-5 text-black">Start, switch, or advance your career with more than 5,400 courses,<br /> Professional Certificates, and degrees from world-class universities <br /> and companies.</p>
+                            <div className="buttons d-flex justify-content-left">
+                                <NavLink to="/register" className="register-btn-big ">Join for Free</NavLink>
+                                <NavLink to="/courses" className="courses-btn-big ">Courses</NavLink>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <About />
-            <Services />
+            {/* <Services /> */}
             <Contact />
         </div>
     )
