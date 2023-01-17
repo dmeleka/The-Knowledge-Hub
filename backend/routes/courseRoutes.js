9e80a8bb5a98809c00312b7524ce2ea683e4e9a7
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2023-01-17 11:30:14 +0100

    modified getSubtitles route

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { verifyJWT } from "../controllers/authController.js";
 import { getAllCourses, getExam, getSubtitles, getVideo, getExams } from '../controllers/courseController.js';
 
 const router = express.Router();
@@ -6,7 +7,7 @@ const router = express.Router();
 router.get('/allCourses', getAllCourses);
 router.get('/getExam/:CourseTitle/:ExamTitle', getExam)
 router.get('/getVideo/:CourseTitle', getVideo)
-router.get('/getSubtitles/:CourseTitle', getSubtitles)
+router.get('/getSubtitles/:CourseTitle', verifyJWT, getSubtitles)
 router.get('/getExams/:CourseTitle', getExams)
 
 
