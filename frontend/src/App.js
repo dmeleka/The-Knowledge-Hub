541f4bcc7677fa741fcf9edcccb3755a2b066182
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2023-01-17 11:28:31 +0100

    modified frontend routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from './components/Home/Home';
-import Navbar from './components/Home/Navbar'
-import { Routes, Route, useParams } from 'react-router-dom';
+// import Navbar from './components/Home/Navbar'
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/User/Login';
 import Footer from './components/Home/Footer';
 import Register from './components/User/Register';
 import TraineeHome from './components/Trainee/TraineeHome'
+import TraineeCourses from './components/Trainee/TraineeCourses'
 import InstructorHome from './components/Instructor/InstuctorHome'
 import AdminHome from './components/Admin/AdminHome';
 import ForgotPassword from './components/User/ForgotPassword';
@@ -14,6 +15,7 @@ import Courses from './components/Courses/Courses';
 import Course from './components/Courses/Course';
 import SearchResults from './components/Courses/SearchResults';
 import Exam from './components/Courses/Exam'
+import Terms from './components/Home/Terms';
 
 function App() {
   return (
@@ -23,7 +25,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/traineeHome" element={<TraineeHome />} />
+        <Route path="/:username" element={<TraineeHome />} />
+        <Route path="/:username/myCourses" element={<TraineeCourses />} />
         <Route path="/instructorHome" element={<InstructorHome />} />
         <Route path="/adminHome" element={<AdminHome />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
@@ -31,8 +34,8 @@ function App() {
         <Route path="/searchResults/:search" element={<SearchResults />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/course/:title" element={<Course />} />
-        {/* <Route path="/course/:title/:exercise" element={<MCQ />} /> */}
         <Route path="/exam/:CourseTitle/:ExamTitle" element={<Exam />} />
+        <Route path="/T&C" element={<Terms />} />
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
