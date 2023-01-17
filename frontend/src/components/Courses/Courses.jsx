295698bf78e14e2157b08abe6731c64382c5191e
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2023-01-17 11:30:45 +0100

    minor adjustment to course card

--- frontend/src/components/Courses/Courses.jsx
+++ frontend/src/components/Courses/Courses.jsx
@@ -35,7 +35,7 @@ const Courses = () => {
                     <img className="card-img-top" src="/course Photos/python.png" alt="Card image cap" />
                     <div className="card-body">
                         <h5 className='card-text'>{c.Title}</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text">{c.ShortSummary}</p>
                     </div>
                 </NavLink>
                 <div class="card-footer">
